bb67e57ba54c3c7acc2c6e67421fcae1e2e9a16e
radopaulen <31773409+radopaulen@users.noreply.github.com>
2024-02-09 09:16:57 +0100

    Update makeoptions.mk

--- MC++_1.0/src/makeoptions.mk
+++ MC++_1.0/src/makeoptions.mk
@@ -1,13 +1,13 @@
 # THIRD-PARTY LIBRARIES <<-- CHANGE AS APPROPRIATE -->>
 
-PATH_PROFIL  = /home/bchachua/Programs/ThirdParty/Profil-2.0.8
-LIB_PROFIL = -L$(PATH_PROFIL)/lib -lProfilPackages -lProfil -lBias -llr
-INC_PROFIL = -I$(PATH_PROFIL)/include
+# PATH_PROFIL  = /home/bchachua/Programs/ThirdParty/Profil-2.0.8
+# LIB_PROFIL = -L$(PATH_PROFIL)/lib -lProfilPackages -lProfil -lBias -llr
+# INC_PROFIL = -I$(PATH_PROFIL)/include
 
-PATH_FILIB = /opt/filib++
-LIB_FILIB = -L$(PATH_FILIB)/lib -lprim
-INC_FILIB = -I$(PATH_FILIB)/include/
-FLAGS_FILIB = -frounding-math -ffloat-store
+# PATH_FILIB = /opt/filib++
+# LIB_FILIB = -L$(PATH_FILIB)/lib -lprim
+# INC_FILIB = -I$(PATH_FILIB)/include/
+# FLAGS_FILIB = -frounding-math -ffloat-store
 
 LIB_LAPACK = -llapack
 LIB_CPPUNIT = -lcppunit
